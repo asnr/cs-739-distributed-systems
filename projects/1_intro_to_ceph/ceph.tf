a24b8e96a6e0c562186d9f10f99e6deb3cf03fc7
asnr <asnr@users.noreply.github.com>
2019-02-17 23:45:21 -0800

    proj1: initial configuration of gcp compute using ansible

--- projects/1_intro_to_ceph/ceph.tf
+++ projects/1_intro_to_ceph/ceph.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "ceph_admin" {
   }
 
   provisioner "remote-exec" {
-    inline = ["echo Hello World!"]
+    inline = ["echo ssh is up and accepting connections!"]
 
     connection {
       type = "ssh"
@@ -32,10 +32,23 @@ resource "google_compute_instance" "ceph_admin" {
       private_key = "${file("${var.ssh_private_key}")}"
     }
   }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_CONFIG=ansible/ansible.cfg ansible-playbook -u ${var.gcp_remote_user} -i '${self.network_interface.0.access_config.0.nat_ip},' --private-key ${var.ssh_private_key} --extra-vars 'hosts_var=all' ansible/ceph_admin.yaml"
+  }
+}
+
+variable "ceph_node_names" {
+  default = {
+    "0" = "ceph-node1"
+    "1" = "ceph-node2"
+    "2" = "ceph-node3"
+  }
 }
 
 resource "google_compute_instance" "ceph_node" {
-  name = "ceph-node"
+  count = 3
+  name = "${lookup(var.ceph_node_names, count.index)}"
   machine_type = "n1-standard-1"
   zone = "us-west1-b"
 
@@ -51,5 +64,19 @@ resource "google_compute_instance" "ceph_node" {
     access_config {
       // Ephemeral IP
     }
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo Hello World!"]
+
+    connection {
+      type = "ssh"
+      user = "${var.gcp_remote_user}"
+      private_key = "${file("${var.ssh_private_key}")}"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_CONFIG=ansible/ansible.cfg ansible-playbook -u ${var.gcp_remote_user} -i '${self.network_interface.0.access_config.0.nat_ip},' --private-key ${var.ssh_private_key} --extra-vars 'hosts_var=all' ansible/ceph_node.yaml"
   }
 }
